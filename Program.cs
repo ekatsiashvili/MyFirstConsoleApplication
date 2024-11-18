2b9d20f61ef061f60f58dbc13518eb90f03c2d62
ekatsiashvili <katsiashvili.elena@gmail.com>
2024-11-18 14:50:43 +0000

    Revert "an error commit"
    This reverts commit 17d0d77b90a653f71be6f185b8b9e59c8f94db13.

--- Program.cs
+++ Program.cs
@@ -11,7 +11,6 @@ namespace MyFirstConsoleApplication
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
-            Console.WriteLine("Feature branch 1 - development 2");
         }
     }
 }
