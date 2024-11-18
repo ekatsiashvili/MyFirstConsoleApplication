23b25ab5d6283e648b4b343abaf8c4bd6b1cd7ef
ekatsiashvili <katsiashvili.elena@gmail.com>
2024-11-18 14:49:27 +0000

    an error commit

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ namespace MyFirstConsoleApplication
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello world!");
+            Console.WriteLine("Feature branch 1 - development 2");
             Console.WriteLine("Feature branch 1 - development 1");
         }
     }
